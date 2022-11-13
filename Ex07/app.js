41cd209324597836856f2340592906338492db86
Igor Minakawa Muraoka <igormuraoka97@gmail.com>
2022-11-13 17:29:56 -0300

    Add Ex07

---
+++ Ex07/app.js
@@ -0,0 +1,8 @@
+async function precoBitcoin () {
+    
+    const response = await fetch ('https://api.coincap.io/v2/assets/bitcoin');
+    const data = await response.json();
+    console.log(`O preço do Bitcoin - ${data.data.symbol} em dólares hoje é $ ${data.data.priceUsd}`);
+}
+
+precoBitcoin();
